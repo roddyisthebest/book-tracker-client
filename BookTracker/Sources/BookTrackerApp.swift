b1d5e3564c6594dc14a4bad2abd38ad4a77ce492
shy-lemon <shy@lemoncloud.io>
2026-02-02 18:01:17 +0900

    chore: bootstrap tuist project

---
+++ BookTracker/Sources/BookTrackerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project.swift
@@ -0,0 +1,33 @@
+import ProjectDescription
+
+let project = Project(
+    name: "BookTracker",
+    targets: [
+        .target(
+            name: "BookTracker",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.seong.booktracker",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["BookTracker/Sources/**"],
+            resources: ["BookTracker/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "BookTrackerTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "dev.seong.booktrackerTests",
+            infoPlist: .default,
+            sources: ["BookTracker/Tests/**"],
+            dependencies: [.target(name: "BookTracker")]
+        ),
+    ]
+)
